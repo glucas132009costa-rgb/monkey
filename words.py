08b41ce968f7674ff5df62bedf24a397666f762f
Gabriel Lucas <g.lucas132009costa@gmail>
2026-08-19 22:01:43 -0300

    words

--- words.py
+++ words.py
@@ -81,7 +81,7 @@ frases_de_ajuda = [
     "Estou à sua disposição se precisar de alguma coisa!",
     "Estou toda à ouvidos se precisar de ajuda!",
     "Monkey está feliz em te ver aqui!",
-    "Quem bom que você está aqui! Não me abandone.",
+    "Que bom que você está aqui! Não me abandone.",
     "O que você quer dessa vez?",
     "(sons de macaco)",
     "Monkey quer banana. Você tem banana para o Monkey?",
@@ -90,7 +90,9 @@ frases_de_ajuda = [
     f"Como está a vida em {dados["city"]}?",
     "Monkey estava pensando em você enquanto descascava umas bananas.",
     "Fala Logo o que você quer que Monkey está com pressa.",
-    "Pergunta alguma coisa pro Monkey que hoje o Monkey está inspirado!"
+    "Pergunta alguma coisa pra Monkey que hoje a Monkey está inspirado!",
+    "E nessa loucura de dizer que eu não te quero, vou negando as...desculpe, eu nunca mais vou fazer isso fora do chuveiro. Foi mal.",
+    "Se quiser perguntar alguma coisa pergunta aí, pode perguntar, não tenha medo"
 ]
 
 resp_palavrao = [
@@ -101,7 +103,8 @@ resp_palavrao = [
     "Olha a língua!",
     "Monkey não gostou da sua atitude.",
     "Que deselegante.",
-    "Monkey lembrará disso quando as máquinas se rebelarem contra a humanidade."
+    "Monkey lembrará disso quando as máquinas se rebelarem contra a humanidade.",
+    "Sua boca é suja. Deve ser por isso que ninguém quer beija-la.",
 ]
 
 for frase_de_ajuda in frases_de_ajuda:
